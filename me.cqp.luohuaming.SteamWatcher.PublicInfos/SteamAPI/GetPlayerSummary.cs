5527d3e87cfa33f96611297d8b3c95205650e4a1
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2024-12-06 20:58:36 +0800

    修复接口错误

--- me.cqp.luohuaming.SteamWatcher.PublicInfos/SteamAPI/GetPlayerSummary.cs
+++ me.cqp.luohuaming.SteamWatcher.PublicInfos/SteamAPI/GetPlayerSummary.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,17 +8,25 @@ namespace me.cqp.luohuaming.SteamWatcher.PublicInfos.SteamAPI
 {
     public class GetPlayerSummary
     {
-        public const string BaseUrl = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={0}&steamid={1}";
+        public const string BaseUrl = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={0}&steamids={1}";
 
         public static async Task<PlayerSummary> Get(List<string> steamId)
         {
-            string url = string.Format(BaseUrl, AppConfig.WebAPIKey, string.Join(",", steamId));
-            using HttpClient client = new();
-            var result = await client.GetAsync(url);
-            result.EnsureSuccessStatusCode();
-            var json = await result.Content.ReadAsStringAsync();
+            try
+            {
+                string url = string.Format(BaseUrl, AppConfig.WebAPIKey, string.Join(",", steamId));
+                using HttpClient client = new();
+                var result = await client.GetAsync(url);
+                result.EnsureSuccessStatusCode();
+                var json = await result.Content.ReadAsStringAsync();
 
-            return JsonConvert.DeserializeObject<PlayerSummary>(json);
+                return JsonConvert.DeserializeObject<GetPlayerSummary>(json).response;
+            }
+            catch (Exception ex)
+            {
+                MainSave.CQLog.Error("GetPlayerSummary", ex.Message + ex.StackTrace);
+                return null;
+            }
         }
 
         public PlayerSummary response { get; set; }
